ccafe0b020a5f8b857bff2b71a44d843d71c025e
LEESEUNGWON\Leeseungwon <iseungwon239@gmail.com>
2022-10-23 16:48:34 +0900

    ArrayQueue 구현

--- StackAndQueue/main.cpp
+++ StackAndQueue/main.cpp
@@ -7,34 +7,61 @@
 using namespace std;
 
 template<typename T>
-class ListQueue
+class ArrayQueue
 {
 public:
+	ArrayQueue()
+	{
+		_container.resize(100);
+	}
+
 	void push(const T& value)
 	{
-		_container.push_back(value);
+		if (_size == _container.size())
+		{
+			int newSize = max(1, _size * 2);
+			vector<T> newData;
+			newData.resize(newSize);
+
+			for (int i = 0; i < _size; ++i)
+			{
+				int index = (_front + i) % _container.size();
+				newData[i] = _container[index];
+			}
+			_container.swap(newData);
+			_front = 0;
+			_back = _size;
+		}
+
+		_container[_back] = value;
+		_back = (_back + 1) % _container.size();
+		_size++;
 	}
 
 	void pop()
 	{
-		_container.pop_front();
+		_front = (_front + 1) % _container.size();
+		_size--;
 	}
 
 	T& front()
 	{
-		return _container.front();
+		return _container[_front];
 	}
 
-	bool empty() { return _container.empty(); }
-	int size() { return _container.size(); }
+	bool empty() { return _size == 0; }
+	int size() { return _size; }
 
 private:
-	list<T> _container;
+	vector<T> _container;
+	int _front;
+	int _back;
+	int _size;
 };
 
 int main()
 {
-	ListQueue<int> q;
+	ArrayQueue<int> q;
 	for (int i = 0; i < 100; ++i)
 	{
 		q.push(i);
